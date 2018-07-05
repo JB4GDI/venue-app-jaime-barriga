60ff2c29cea6c0c0d7d1954a7024b42db01a99ed
JB4GDI <JB4GDI@gmail.com>
2018-07-05 14:11:35 -0400

    Added a Header to the page, via React

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+  render() {
+    return (
+      <div className="header fl">
+        <h1 className="fl">Arrange Photos</h1>
+        <p  className="username fr">Hello Jaime!</p>
+      </div>
+    );
+  }
+
+}
+
+export default Header;
