e17459941af349bc3633e9c25c98fe582d8c57dc
JB4GDI <JB4GDI@gmail.com>
2018-07-10 16:36:59 -0400

    Don't need these increase/decrease functions anymore

--- src/components/CategoryContainer.js
+++ src/components/CategoryContainer.js
@@ -172,8 +172,6 @@ class CategoryContainer extends React.Component {
           latestSelectedPhotoCategory={this.props.latestSelectedPhotoCategory}
           selectedPhotoIds={this.props.selectedPhotoIds}
           handleSinglePhotoSelect={this.props.handleSinglePhotoSelect}
-          increasePhotosSelected={this.props.increasePhotosSelected}
-          decreasePhotosSelected={this.props.decreasePhotosSelected}
 
           updatePhoto = {this.props.updatePhoto}
 
